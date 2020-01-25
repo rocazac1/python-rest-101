0bdd0716e0db4805e84c556e73cff0c5f18533f6
Robert <robert@tgl-dhcp-128-76-39-247.dk.eng.tdc.net>
2020-01-25 20:11:06 +0100

    restrict thello endpoint to http get only

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/hello")
+@app.route("/hello", methods=['GET'])
 def hello():
     name = request.args.get('name')
     if name:
