7da7e983e05644125e1b434cde719401141e4df8
darren-rose <darrenwrose@gmail.com>
2020-01-26 17:57:59 +0100

    polish tests

--- src/test_models.py
+++ src/test_models.py
@@ -1,10 +1,10 @@
 import unittest
 from models import Person
 
-class testPerson(unittest.TestCase):
+class TestPerson(unittest.TestCase):
     def test_str(self):
         actual = Person("Robert", 27).__str__()
-        self.assertEqual('name = Robert age = 27', actual)
+        self.assertEqual('name = Robert, age = 27', actual)
     def test_isAdult(self):
         actual = Person("Robert", 27).isAdult()
         self.assertEqual(True, actual)
