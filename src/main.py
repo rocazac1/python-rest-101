ec78ab2b2857e9a0d3b0bad932d6824b0370859a
Robert <robert@MacBook-Pro.localdomain>
2020-01-26 19:16:52 +0100

    fixed linting

--- src/main.py
+++ src/main.py
@@ -2,17 +2,20 @@ from flask import Flask, request
 from waitress import serve
 from paste.translogger import TransLogger
 import logging
-import json
 import os
+
 from models import Person
+
 
 app = Flask(__name__)
 logger = logging.getLogger('waitress')
 logging.basicConfig(level=logging.DEBUG, filemode="w", filename=os.getenv('LOG_DIR', '.') + "/app.log")
 
+
 @app.route("/health", methods=['GET'])
 def health():
     return ("OK")
+
 
 @app.route("/hello", methods=['GET'])
 def hello():
@@ -33,6 +36,7 @@ def createToDos():
         return (person.toJson())
     except (ValueError, KeyError, TypeError):
       return "Invalid Request", 400
+
 
 if __name__  == '__main__':
     serve(TransLogger(app, setup_console_handler = True), host='0.0.0.0', port=5000)
