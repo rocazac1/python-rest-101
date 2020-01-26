968ebb530a886a64d305dbd54c1fecf6ce781011
darren-rose <darrenwrose@gmail.com>
2020-01-26 17:49:07 +0100

    add env var to docker run

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-docker run --rm -p  5000:5000 -v $(pwd)/log:/var/log rocazac1/python-rest-101 
+docker run --rm -p 5000:5000 -e LOG_DIR=/var/log -v $(pwd)/log:/var/log rocazac1/python-rest-101 
